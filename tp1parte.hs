9034aa9c649f44dfed8e7ff93090fa58827b2b9d
Unknown <matiasblacutt16@gmail.com>
2018-04-10 22:35:51 -0300

    Se definio el tipo de 'transaccion' y se modifico para que devuelva un evento, en vez de un usuario

--- tp1parte.hs
+++ tp1parte.hs
@@ -1,4 +1,3 @@
---07/04/2018
 
 {-# LANGUAGE NoMonomorphismRestriction #-}
 import Text.Show.Functions
@@ -17,6 +16,8 @@ pepe = Usuario "Jose" 10
 lucho = Usuario "Luciano" 2
 pepe2 = Usuario "Jose" 50
 
+type Evento = Usuario -> Usuario
+
 evento nuevabilletera unusuario = unusuario { billetera=nuevabilletera}
 comominimo0 numero = max numero 0
 comomaximo10 numero = min numero 10
@@ -31,24 +32,24 @@ quedaigual unusuario = evento (billetera unusuario) unusuario
 
 -------------------------Transacciones--------------------------------
 
---Aclaracion: falta definir el tipo de transaccion
---type Transaccion = 
+transaccion :: Evento-> Usuario-> Usuario-> Evento
 
-transaccion evento nombreUsuario usuario  |  nombreUsuario == nombre usuario = evento usuario
-										  |  otherwise = quedaigual usuario
+transaccion evento usuarioAlQueSeLeDebeAplicarLaTransaccion usuarioAlQueSeLeIntentaAplicarLaTransaccion
+  |  nombre usuarioAlQueSeLeDebeAplicarLaTransaccion == nombre usuarioAlQueSeLeIntentaAplicarLaTransaccion = evento 
+  |  otherwise = quedaigual 
 
 --Tests Transacciones 
-transaccion1 usuario = transaccion cierredecuenta "Luciano" usuario
-transaccion2 usuario = transaccion (deposito 5) "Jose" usuario
+transaccion1 = transaccion cierredecuenta lucho 
+transaccion2 = transaccion (deposito 5) pepe 
 
 ejecutarTestTransacciones = hspec $ do 
 	transaccion1EnPepe 
 	transaccion2EnPepe
 	transaccion2EnPepe2
 
-transaccion1EnPepe = it "La billetera de pepe queda igual al querersele realizar la transaccion: Lucho cierra la cuenta" (billetera (transaccion1 pepe) `shouldBe` billetera pepe)
-transaccion2EnPepe = it "Transaccion 'Pepe deposita 5 monedas' en Pepe. Billetera inicial: 10, Billetera final: 15" (billetera (transaccion2 pepe) `shouldBe` (billetera pepe)+5)
-transaccion2EnPepe2 = it "Transaccion 'Pepe deposita 5 monedas' en Pepe2. Billetera inicial: 50, Billetera final: 55" (billetera (transaccion2 pepe2) `shouldBe` (billetera pepe2)+5)
+transaccion1EnPepe = it "La billetera de pepe queda igual al querersele realizar la transaccion: 'Lucho cierra la cuenta'" (billetera ((transaccion1 pepe) pepe)  `shouldBe` billetera pepe)
+transaccion2EnPepe = it "Transaccion 'Pepe deposita 5 monedas' en Pepe. Billetera inicial: 10, Billetera final: 15" (billetera ((transaccion2 pepe) pepe) `shouldBe` (billetera pepe)+5)
+transaccion2EnPepe2 = it "Transaccion 'Pepe deposita 5 monedas' en Pepe2. Billetera inicial: 50, Billetera final: 55" (billetera ((transaccion2 pepe2) pepe2) `shouldBe` (billetera pepe2)+5)
 
 
 --------------------------NuevosEventos------------------------------
@@ -56,15 +57,15 @@ transaccion2EnPepe2 = it "Transaccion 'Pepe deposita 5 monedas' en Pepe2. Billet
 tocoMeVoy usuario =  (cierredecuenta.upgrade.deposito 15) usuario
 ahorranteErrante usuario = (deposito 10.upgrade.deposito 8.extraccion 1.deposito 2.deposito 1) usuario
 
---Test Transacciones
+--Test Nuevos Eventos
 
-transaccion3 usuario = transaccion tocoMeVoy "Luciano" usuario
-transaccion4 usuario = transaccion ahorranteErrante "Luciano" usuario
+transaccion3 = transaccion tocoMeVoy lucho2
+transaccion4 = transaccion ahorranteErrante lucho2
 lucho2 = Usuario "Luciano" 10 --creo otro porque se pide probarlo en una billetera de 10
 
 ejecutarTestNuevosEventos = hspec $ do
 	transaccion3EnLucho
 	transaccion4EnLucho
 
-transaccion3EnLucho = it "Lucho toca y se va. Su billetera queda en 0" (billetera (transaccion3 lucho2) `shouldBe` 0)
-transaccion4EnLucho = it "Lucho es un ahorrante errante. Billetera inicial: 10 Billetera final: 34" (billetera (transaccion4 lucho2) `shouldBe` 34)
+transaccion3EnLucho = it "Transaccion: 'Lucho toca y se va' en Lucho. Su billetera queda en 0" (billetera ((transaccion3 lucho2) lucho2) `shouldBe` 0)
+transaccion4EnLucho = it "Transaccion: 'Lucho es un ahorrante errante' en Lucho. Billetera inicial: 10 Billetera final: 34" (billetera ((transaccion4 lucho2) lucho2) `shouldBe` 34)
